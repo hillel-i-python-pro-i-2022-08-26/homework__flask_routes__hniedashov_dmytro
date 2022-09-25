48a3459a7125cee18bdcfc917e0e07e16c875cb6
dmytro <dHniedashov@spinemedia.com>
2022-09-25 21:50:38 +0300

    Added finished space section

--- application/main/controllers/space_controller.py
+++ application/main/controllers/space_controller.py
@@ -1,8 +1,15 @@
 from flask import Blueprint
+from application.main.services.space_service import SpaceService
 
 blueprint = Blueprint("space", __name__, url_prefix="/space")
+space_service = SpaceService()
 
 
 @blueprint.route("/")
-def index():
-    return "SPACE"
+def astros():
+    return space_service.get_astros()
+
+
+@blueprint.route("/location")
+def location():
+    return space_service.get_current_location()
